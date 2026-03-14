48b20b1b92c16b3730841a20eee87a3da65430d1
Lua T. Andrade <andradelua2004@gmail.com>
2026-03-14 00:23:19 -0300

    Got the server up and running!!!

--- src/server_api/core.clj
+++ src/server_api/core.clj
@@ -1,7 +1,13 @@
 (ns server-api.core
+  (:require [server-api.system.server :as server]
+            [com.stuartsierra.component :as component])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "The main function."
+  [port]
+  (-> (component/start
+       (component/system-map :web-server ;; Hash-map of all active components
+                             (server/web-server port)))))
+
+
